70caa978bb1cbd64139bb61d79dc086807f4f058
Henson Dannhorn <hdannhorn3645@student.pps.net>
2026-05-19 15:33:07 -0700

    updated tileset, added shift functionality, slight map updates

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
+const SHIFT_SPEED = 100.0
 const JUMP_VELOCITY = -335.0
 
 @onready var CoyoteTimer = SceneTreeTimer
@@ -39,9 +40,15 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
-		velocity.x = direction * SPEED
+		if Input.is_action_pressed("Shift") and (is_on_floor() or CoyoteActive):
+			velocity.x = direction * SHIFT_SPEED
+		else:
+			velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if Input.is_action_pressed("Shift") and (is_on_floor() or CoyoteActive):
+			velocity.x = move_toward(velocity.x, 0, SHIFT_SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	move_and_slide()
 	
