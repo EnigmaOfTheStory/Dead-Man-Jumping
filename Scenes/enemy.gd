11c4121562c976b7db39910181b7bd016f9a96df
Henson Dannhorn <hdannhorn3645@student.pps.net>
2026-05-29 15:34:10 -0700

    updated movement slightly

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -4,18 +4,23 @@ var player
 var enemy
 var speed
 var direction
+var distance
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_parent().get_node("Player")
-	speed = 30
+	speed = 45
 	enemy = self
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	direction = (enemy.global_position - player.global_position).normalized()
-	velocity = speed * -direction
+	distance = (enemy.global_position - player.global_position).length()
+	if distance <= 300:
+		velocity = speed * -direction
+	else:
+		velocity = 0 * -direction
 	#print(direction)
 	
 	
